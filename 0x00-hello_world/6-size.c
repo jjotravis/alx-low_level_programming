612c3c9618749e56d8e700d8e5ba1e2bf7b21a5d
Travis Jack <ondegotravis@gmail.com>
2022-09-08 23:34:58 +0300

    Size of var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Return : Always (0) success
+ * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	char a;
 	int b;
@@ -19,5 +20,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
-	return(0);
+	return (0);
 }
