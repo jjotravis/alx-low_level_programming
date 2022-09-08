ed99908b1bc4be07016fb50eb47b402ff88fc492
Travis Jack <ondegotravis@gmail.com>
2022-09-08 22:54:49 +0300

    Size of var

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * introduces and sets the value of variables
+ * prints the var size
+ * Return : always 0 success
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("The size of char is:%lu.\n", (unsigned long)sizeof(a));
+	printf("The size of int is:%lu.\n", (unsigned long)sizeof(b));
+	printf("The size of long int is:%lu.\n", (unsigned long)sizeof(c));
+	printf("The size of long long int is:%lu.\n", (unsigned long)sizeof(d));
+	printf("The size of float is:%lu.\n", (unsigned long)sizeof(e));
+
+	return(0);
+}
