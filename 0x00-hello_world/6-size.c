4a29a1e199be1f5d4d94e495787dc9b37cb4604e
Travis Jack <ondegotravis@gmail.com>
2022-09-08 22:59:01 +0300

    Size of var byte(s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("The size of char is:%lu.\n", (unsigned long)sizeof(a));
-	printf("The size of int is:%lu.\n", (unsigned long)sizeof(b));
-	printf("The size of long int is:%lu.\n", (unsigned long)sizeof(c));
-	printf("The size of long long int is:%lu.\n", (unsigned long)sizeof(d));
-	printf("The size of float is:%lu.\n", (unsigned long)sizeof(e));
+	printf("The size of char is:%lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("The size of int is:%lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("The size of long int is:%lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("The size of long long int is:%lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("The size of float is:%lu byte(s)\n", (unsigned long)sizeof(e));
 
 	return(0);
 }
