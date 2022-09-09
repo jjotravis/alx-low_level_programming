f915afba7933232fcae79c103b2559adb57331c5
Travis Jack <ondegotravis@gmail.com>
2022-09-09 23:53:15 +0300

    Double digit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints 2 digit numbers
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i / 100) + '0');
+
+		if (i != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
