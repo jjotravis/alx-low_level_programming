aab22ed342ffb8e5d484a60c8072534e7e805050
Travis Jack <ondegotravis@gmail.com>
2022-09-13 21:10:59 +0300

    Prints alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - void no return value
+ *
+ * Description: prints alphabet in lower case
+ * Return: Nothing
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
