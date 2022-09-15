6380c512cccfd1c9cfb33ee3edd8c6afb396799d
Travis Jack <ondegotravis@gmail.com>
2022-09-16 00:19:24 +0300

    prints numbers x10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14
+ * ten times
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			else
+				_putchar((j % 10) + '0');
+		}
+			_putchar(10);
+	}
+}
