db9d701a9b4391672639d2d756577a26373c551e
Travis Jack <ondegotravis@gmail.com>
2022-09-16 01:00:17 +0300

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
