000bc480de05178bc538109f8d25e4eb366775be
Travis Jack <ondegotravis@gmail.com>
2022-09-22 22:36:49 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
 	int diff = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 && *s2)
 	{
-		if (s1[i] < s2[i])
-		{
-			diff = (s1[i] - s2[i]);
-		}
-		else
-			diff = (s1[i] - s2[i]);
-		i++;
+		diff = (*s1 - *s2);
+		s1++, s2++;
+		return (diff);
 	}
 	return (diff);
 }
