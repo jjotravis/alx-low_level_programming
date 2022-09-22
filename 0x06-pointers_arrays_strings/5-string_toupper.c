78f61c1a529e46f54352ff5b8fabf7813f6123d3
Travis Jack <ondegotravis@gmail.com>
2022-09-22 23:22:18 +0300

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @str: parameter
+ * Return: characters
+ */
+
+char *string_toupper(char *str)
+{
+	while (*str++)
+	{
+		if (*str >= 'a' && *str <= 'z')
+			*str -= 32;
+	}
+	return (str);
+}
