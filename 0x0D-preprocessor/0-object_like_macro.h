5f7e787b591d515fb8bc5a2dffc93025c1c05269
Travis Jack <ondegotravis@gmail.com>
2022-10-12 22:31:18 +0300

    pi macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,5 +1,9 @@
 #ifndef OBJECT_LIKE_MACRO_H
-#define OBECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#endif
+
+#ifndef SIZE
 #define SIZE 1024
 
 #endif
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,9 @@
+#ifndef PI_H
+#define PI_H
+
+#endif
+
+#ifndef PI
+#define PI 3.14159265359
+
+#endif
