f03271a87f578bbe49030ebe9ebecaa391d9ff7c
Travis Jack <ondegotravis@gmail.com>
2022-10-14 10:40:21 +0300

    searches for an integers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array of integers to be scanned
+ * @size: size of the array
+ * @cmp: pointer to function used to compare values
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i <= size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
