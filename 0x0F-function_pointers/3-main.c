fb5e2934eeb8a02be4c6524b944a0b3aff7fb295
Travis Jack <ondegotravis@gmail.com>
2022-10-17 11:34:11 +0300

    correction on mod 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -28,6 +28,11 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
