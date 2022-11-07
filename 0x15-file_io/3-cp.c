1759350a569208228da28712146040d45480c628
Travis Jack <ondegotravis@gmail.com>
2022-11-07 23:39:15 +0300

    copies content from one filr  to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -55,7 +55,7 @@ void close_file(int fd)
  * If file_to cannot be created or written to - exit code 99.
  * If file_to or file_from cannot be closed - exit code 100.
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int from, to, r, w;
 	char *buffer;
