0b00009368eb94b59e7a28bff36ece58e2d5d2c4
Travis Jack <ondegotravis@gmail.com>
2022-12-30 22:34:50 +0300

    review

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	if (new_node == NULL || idx > listint_len(*head))
 		return (NULL);
 	if (*head == NULL)
 	{
@@ -44,4 +44,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
+}
+
+/**
+ * listint_len - counts number of nodes in a list
+ * @h: head of the list
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t count = 0;
+
+	temp = h;
+	while (temp)
+	{
+		count += 1;
+		temp = temp->next;
+	}
+	return (count);
 }
