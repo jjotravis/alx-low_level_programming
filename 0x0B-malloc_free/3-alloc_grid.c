39025986b1db41c67bab838e4c3c3c44d4424216
Travis <ondegotravis@gmail.com>
2023-07-12 00:49:44 +0300

    correction on alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		array[i] = malloc(sizeof(int *) * width);
+		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
 			for (i = i - 1; i >= 0; i--)
