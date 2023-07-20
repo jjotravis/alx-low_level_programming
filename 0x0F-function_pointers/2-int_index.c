6760a8477aa914e3f54f47193690fd1fe5eefe24
Travis <ondegotravis@gmail.com>
2023-07-20 16:38:38 +0300

    Function Pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array of integers to be scanned
+ * @size: size of the array
+ * @cmp: pointer to function used to compare values
+ *
+ * Return: if no elements match return -1
+ * if size <= 0, return -1
+ * else returns index for element for cmp not 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i <= size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - operation that adds two integers
+ * @a: 1st integer
+ * @b: 2nd integers
+ *
+ * Return: result of addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operation that subtracts 1 integer from another
+ * @a: 1st integer
+ * @b: 2nd integer to be subtracted
+ *
+ * Return: result of subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operation that multiplies two numbes
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: product of the numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operation that multiplies two numbers
+ * @a: number to be divided
+ * @b: the divisor
+ *
+ * Return: the result of the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operation that gives the mod of a number
+ * @a: number to be divided
+ * @b: the divisor
+ *
+ * Return: the remainder for the division
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
