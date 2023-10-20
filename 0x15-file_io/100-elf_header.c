71259b98b134f230713a2a3327a4486047ee16df
Travis <ondegotravis@gmail.com>
2023-10-20 22:50:14 +0300

    elf again

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,88 +1,159 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-#include <stdlib.h>
+#include "main.h"
 
-#define BUFFER_SIZE 128
 /**
- * error_exit - Entry point
- * @message: string
- * Return: void
- */
-void error_exit(char *message)
+* type - print the type
+* @ident: the ELF struct
+* @type: data to compare and print.
+* Return: Void
+*/
+
+void type(unsigned int type, unsigned char *ident)
 {
-	fprintf(stderr, "%s\n", message);
-	exit(98);
+	ident[EI_DATA] == ELFDATA2MSB ? type = type >> 8 : type;
+
+	printf("Type:							  ");
+	if (type == ET_NONE)
+		printf("NONE (Unknown type)\n");
+	else if (type == ET_REL)
+		printf("REL (Relocatable file)\n");
+	else if (type == ET_EXEC)
+		printf("EXEC (Executable file)\n");
+	else if (type == ET_DYN)
+		printf("DYN (Shared object file)\n");
+	else if (type == ET_CORE)
+		printf("CORE (Core file)\n");
+	else
+		printf("<unknown: %x>\n", type);
 }
 
 /**
- * print_elf_header_info - Print elf header info
- * @header: header
- * Return: void
+ * osabi - print the osabi
+ * @ident: the ELF struct
+ * return: no return is a void func.
  */
-void print_elf_header_info(Elf64_Ehdr *header)
+void osabi(unsigned char *ident)
+{
+	printf("OS/ABI:							");
+	if (ident[EI_OSABI] == ELFOSABI_SYSV)
+		printf("UNIX - System V\n");
+	else if (ident[EI_OSABI] == ELFOSABI_HPUX)
+		printf("UNIX - HP-UX\n");
+	else if (ident[EI_OSABI] == ELFOSABI_NETBSD)
+		printf("UNIX - NetBSD\n");
+	else if (ident[EI_OSABI] == ELFOSABI_LINUX)
+		printf("UNIX - Linux\n");
+	else if (ident[EI_OSABI] == ELFOSABI_SOLARIS)
+		printf("UNIX - Solaris\n");
+	else if (ident[EI_OSABI] == ELFOSABI_IRIX)
+		printf("UNIX - IRIX\n");
+	else if (ident[EI_OSABI] == ELFOSABI_FREEBSD)
+		printf("UNIX - FreeBSD\n");
+	else if (ident[EI_OSABI] == ELFOSABI_TRU64)
+		printf("UNIX - TRU64\n");
+	else if (ident[EI_OSABI] == ELFOSABI_ARM)
+		printf("ARM\n");
+	else if (ident[EI_OSABI] == ELFOSABI_STANDALONE)
+		printf("Standalone App\n");
+	else
+		printf("<unknown: %x>\n", ident[EI_OSABI]);
+}
+
+
+
+
+/**
+ * displayElfHeader - Function to display ELF header information
+ * @header: file to display elf information
+ * Return - void
+*/
+void displayElfHeader(ElfHeader *header)
 {
 	int i;
 
-	printf("Magic:   ");
-	for (i = 0; i < EI_NIDENT; i++)
+	printf("Magic: ");
+	for (i = 0; i < 16; i++)
 	{
-		printf("%02x ", header->e_ident[i]);
+		printf("%02x", header->ident[i]);
+		if (i < 15)
+			printf(" ");
 	}
-	printf("\n");
-	printf("Class:							 %s\n",
-			header->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" : "ELF32");
-	printf("Data:							  %s\n",
-			header->e_ident[EI_DATA] == ELFDATA2LSB ?
-			"2's complement, little endian" : "2's complement, big endian");
-	printf("Version:						   %d (current)\n",
-			header->e_ident[EI_VERSION]);
-	printf("OS/ABI:							%d\n",
-			header->e_ident[EI_OSABI]);
-	printf("ABI Version:					   %d\n",
-			header->e_ident[EI_ABIVERSION]);
-	printf("Type:							  %d\n", header->e_type);
-	printf("Entry point address:			   0x%lx\n", header->e_entry);
+	printf("\nClass:							  %s\n", header->ident[4] == 1 ? "ELF32" : "ELF64");
+	printf("Data:							   %s\n", header->ident[5] == 1 ?
+		"2's complement, little-endian" : "2's complement, big-endian");
+	printf("Version:							%d (current)\n", header->ident[6]);
+	/*printf("OS/ABI:							 %d\n", header->ident[7]);*/
+	osabi(header->ident);
+	printf("ABI Version:						%d\n", header->ident[8]);
+	/*printf("Type:							   0x%04x\n", header->type);*/
+	type(header->type, header->ident);
+	printf("Machine:							0x%04x\n", header->machine);
+	printf("Version:							0x%08x\n", header->version);
+	printf("Entry point address:				0x%08x\n", header->entry);
+	printf("Start of program headers:		  %lu (bytes into file)\n",
+		(unsigned long)header->phoff);
+	printf("Start of section headers:		  %lu (bytes into file)\n",
+		(unsigned long)header->shoff);
+	printf("Flags:							 0x%08x\n", header->flags);
+	printf("Size of this header:			   %u (bytes)\n", header->ehsize);
+	printf("Size of program headers:		   %u (bytes)\n", header->phentsize);
+	printf("Number of program headers:		 %u\n", header->phnum);
+	printf("Size of section headers:		   %u (bytes)\n", header->shentsize);
+	printf("Number of section headers:		 %u\n", header->shnum);
+	printf("Section header string table index: %u\n", header->shstrndx);
 }
 
 /**
- * main - Entry point
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
+ * main - Displays the information contained in the
+ * ELF header at the start of an ELF file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the file is not an ELF File or
+ * the function fails - exit code 98.
  */
 int main(int argc, char *argv[])
 {
+	const char *filename;
 	int fd;
-	Elf64_Ehdr header;
+	ElfHeader elfHeader;
 
 	if (argc != 2)
 	{
-		error_exit("Usage: elf_header elf_filename");
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
 	}
 
-	fd = open(argv[1], O_RDONLY);
+	filename = argv[1];
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 	{
-		error_exit("Error opening file");
+		perror("Error opening file");
+		return (98);
 	}
 
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	/* Read the ELF header */
+	if (read(fd, &elfHeader, sizeof(elfHeader)) != sizeof(elfHeader))
 	{
-		error_exit("Error reading ELF header");
+		perror("Error reading ELF header");
+		close(fd);
+		return (98);
 	}
 
-	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
-		header.e_ident[EI_MAG1] != ELFMAG1 ||
-		header.e_ident[EI_MAG2] != ELFMAG2 ||
-	header.e_ident[EI_MAG3] != ELFMAG3)
+	/*Check if it's a valid ELF file*/
+	if (memcmp(elfHeader.ident, "\x7F\x45\x4C\x46", 4) != 0)
 	{
-		error_exit("Not an ELF file");
+		fprintf(stderr, "Not a valid ELF file: %s\n", filename);
+		close(fd);
+		return (98);
 	}
 
-	print_elf_header_info(&header);
+	/* Display ELF header information*/
+	displayElfHeader(&elfHeader);
 
 	close(fd);
 	return (0);
 }
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <elf.h>
+
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filenam, size_t letters);
@@ -10,4 +18,23 @@ int append_text_to_file(const char *filename, char *text_content);
 int main(int argc, char *argv[]);
 void close_file(int fd);
 char *create_buffer(char *file);
+
+/* ELF header struct*/
+typedef struct 
+{
+	unsigned char ident[16];
+	uint16_t type;
+	uint16_t machine;
+	uint32_t version;
+	uint32_t entry;
+	uint32_t phoff; /*program headers*/
+	uint32_t shoff; /*section headers*/
+	uint32_t flags;
+	uint16_t ehsize;
+	uint16_t phentsize;
+	uint16_t phnum;
+	uint16_t shentsize;
+	uint16_t shnum;
+	uint16_t shstrndx;
+} ElfHeader;
 #endif
